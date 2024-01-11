ce4dd9d3bac9a741fdeca0f053907e7d386c23ed
firman-qs <firmansabil10@gmail.com>
2024-01-11 16:08:26 +0700

    xrdb merge and disable dunst image

--- .local/bin/setwall.sh
+++ .local/bin/setwall.sh
@@ -2,5 +2,6 @@
 
 # nitrogen --set-zoom-fill $1 &&
 wal -i $1 &&
+xrdb -merge ~/.Xresources &&
 xdotool key Super+Shift+F5 &
-convert $1 -crop 700x700+10+5 -gravity Center /home/fqs/Pictures/notification/notifimg.jpg &
+# convert $1 -crop 700x700+10+5 -gravity Center /home/fqs/Pictures/notification/notifimg.jpg &
