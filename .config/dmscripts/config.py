b0333e3240f20b143ae9988256e74422c240513e
firman-qs <firmansabil10@gmail.com>
2024-01-12 20:16:02 +0700

    add distrotube dmenu scripts

---
+++ .config/dmscripts/config.py
@@ -0,0 +1,18 @@
+'''
+------------------------------------------------------------------------------
+author      = "github.com/firman-qs"
+version     = "1.0.0"
+date        = "16-08-2023"
+------------------------------------------------------------------------------
+'''
+
+def dmenu(prompt):
+    '''run dmenu'''
+    dmenu_conf = ["dmenu", "-i",
+             # "-l", "10",
+             # "-g", "1",
+             "-p", prompt]
+    return dmenu_conf
+
+BACKGROUND_DIRERCTORY = "/home/fqs/Pictures/wallpapers"
+
